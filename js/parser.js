a4ca5838b36af0c5f2d7d304482ffc7193a6bb27
eugene-reim <eugene.reim@gmail.com>
2020-02-01 17:12:25 +0300

    Introduce Full-worked gallery system

--- js/parser.js
+++ js/parser.js
@@ -8,8 +8,15 @@ function readTextFile(file, callback) {
     rawFile.send(null);
 }
 readTextFile("projects/list.json", r => {
-    r = JSON.parse(r)
-    r.map(x => {
-        $('.second-slide .container').append(`<div class='card'><img src='img/placeholder.svg' alt='placeholder' /><h3>${x.title}</h3><p>${x.desc}</p></div>`)
+        JSON.parse(r).map(x => {
+            $('.second-slide .container').append(`<div class='card'><img src='${x.img}' alt='placeholder' /><h3>${x.title}</h3> <a href="${x.link}" title="See Repo"><img class="btn mx-auto my-auto" alt="See Repo" src="img/ic-eye.svg"></a><p>${x.desc}</p></div>`)
+        })
     })
-})
+    /*
+     <div class="card">
+        <img src="img/placeholder.svg" alt="placeholder" />
+        <h3>No title</h3>
+        <a href="https://github.com" title="See Repo"><img class="btn mx-auto my-auto" href="github.com" alt="See Repo" src="img/ic-eye.svg"></a>
+        <p>In development</p>
+    </div>
+    */
