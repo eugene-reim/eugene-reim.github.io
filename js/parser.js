296106cce3a6ad33de1e4d8549d57b598546fe03
eugene-reim <eugene.reim@gmail.com>
2020-02-01 16:22:47 +0300

    Updated parser

--- js/parser.js
+++ js/parser.js
@@ -8,5 +8,6 @@ function readTextFile(file, callback) {
     rawFile.send(null);
 }
 readTextFile("projects/list.json", r => {
+    $('.second-slide .container').append(`<div class='card'><img src='img/placeholder.svg' alt='placeholder' /><h3>${r[0].title}</h3><p>${r[0].desc}</p></div>`)
     console.log(r)
 })
